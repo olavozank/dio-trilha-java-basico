78893e34bc8f6a4beb1f5c6514cc2630b9b565b3
Olavo Zank <olavozank@gmail.com>
2024-05-06 15:28:46 -0300

    feat: add ContaTerminal

--- Conta Banco/src/ContaTerminal.java
+++ Conta Banco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner tc = new Scanner(System.in);
+
+        System.out.println("Olá, por favor digite o número da conta");
+        int numero = tc.nextInt();
+        tc.nextLine();
+
+        System.out.println("Agora digite o Número da sua Agencia");
+        String agencia = tc.nextLine();
+
+        System.out.println("Agora digite o seu nome completo");
+        String nome = tc.nextLine();
+        
+        System.out.println("Agora informe o seu saldo em conta");
+        double saldo = tc.nextDouble();
+
+        System.out.println("Olá " + nome +
+        ", obrigado por criar uma conta em nosso banco, sua agência é: "  + agencia + 
+        ", conta: " + numero + " e seu saldo é de: R$" + saldo + " já está disponível para saque");
+
+        tc.close();
+
     }
 }
